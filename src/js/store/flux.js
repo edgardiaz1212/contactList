8b2e66a11d8641391d8341fd4a1e0baeefff7742
Edgar Diaz <ed35376@gmail.com>
2023-05-22 00:06:45 -0400

    store and action data added

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,74 +1,96 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
+  return {
+    store: {
+      BASEURL: `https://assets.breatheco.de/apis/fake/contact`,
 
-		store: {
-			BASEURL: `https://assets.breatheco.de/apis/fake/contact/agenda`,
+	  
+	
 
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
-			contact: {
-				id: "473",
-				agenda_slug: "eduuuuu",
-				full_name: "Eduardo Puermas",
-				email: "epuermas@gmail.com",
-				phone: "305-423-3800",
-				address: "66 W Flagler St #900, Miami, FL 33130",
-				created_at: "2019-11-22 01:02:25"
-			}
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+      contacts: [],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      loadSomeData: () => {
+        /**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
 
-				//reset the global store
-				setStore({ demo: demo });
-			},
-			saludar : () =>{
-				console.log("hola")},
+        //reset the global store
+        setStore({ demo: demo });
+      },
 
+	  //para traer todos los contactos
+      allContacts: async () => {
+        try {
+          let response = await fetch(`${getStore().BASEURL}/agenda/newPulco`);
+
+          if (response.ok) {
+            let data = await response.json();
+            setStore({ contacts: data });
+            console.log(data);
+          } else console.log("error al traer contactos");
+        } catch (err) {
+          console.log(err);
+        }
+      },
+      addNewContact: async (newContact) => {
+        try {
+          let response = await fetch(`${getStore().BASEURL}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(...store.contacts, {
+              id: "",
+              agenda_slug: "",
+              full_name: "newContact.full_name",
+              email: "newContact.email",
+              phone: "newContact.phone",
+              address: "newContact.address",
+              created_at: new Date().toISOString(),
+            }),
+          });
+          if (response.ok) {
+            getActions().allContacts();
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      },
+
+	  updateContatc : async () =>{
+		try {
 			
-		allContacts: async () =>{ {
-			 {
-				try{
-					let response = await fetch(`https://assets.breatheco.de/apis/fake/contact/agenda`);
-					let data = await response.json();
-					console.log(data)
-
-				} catch (err) {
-					console.log(err);
-				}
-			};
+		} catch (error) {
+			console.log(error)
 		}
-	}
-
-}
-}};
+	  }
+    },
+  };
+};
 
 export default getState;
--- src/js/views/AddContact.jsx
+++ src/js/views/AddContact.jsx
@@ -5,115 +5,132 @@ import { Context } from "../store/appContext";
 
 import "../../styles/demo.css";
 
-
-
-
-
 export const AddContact = () => {
   const { store, actions } = useContext(Context);
-  const [contacts, Setcontacts] = useState({})
-  
+  const [inputContacts, setInputContacts] = useState({
+    full_name: "",
+    email: "",
+    phone: "",
+    address: "",
+  });
+
+  const handleInputs = (event) => {
+    const { id, value } = event.target;
+    setInputContacts({ ...inputContacts, [id]: value });
+  };
+  const handleSave = () => {
+    actions.addNewContact(inputContacts);
+  };
+
   return (
-	<>
-    <div className="container">
-      <ul className="list-group">
-        {store.demo.map((item, index) => {
-          return (
-            <li
-              key={index}
-              className="list-group-item d-flex justify-content-between"
-              style={{ background: item.background }}
-            >
-              <Link to={"/single/" + index}>
-                <span>Link to: {item.title}</span>
-              </Link>
-              {
-                // Conditional render example
-                // Check to see if the background is orange, if so, display the message
-                item.background === "orange" ? (
-                  <p style={{ color: item.initial }}>
-                    Check store/flux.js scroll to the actions to see the code
-                  </p>
-                ) : null
-              }
-              <button
-                className="btn btn-success"
-                onClick={() => actions.changeColor(index, "orange")}
+    <>
+      <div className="container">
+        <ul className="list-group">
+          {store.demo.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className="list-group-item d-flex justify-content-between"
+                style={{ background: item.background }}
               >
-                Change Color
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-
-    <div className="container">
-    <h1 className='text-center'>Add a new contact</h1>
-    <div className="mb-3">
-      <label for="fullname" className="form-label">
-        Full name
-      </label>
-      <div className="input-group">
-        <input
-          type="text"
-          className="form-control"
-          id="fullname"
-          placeholder="Fullname"
-        />
+                <Link to={"/single/" + index}>
+                  <span>Link to: {item.title}</span>
+                </Link>
+                {
+                  // Conditional render example
+                  // Check to see if the background is orange, if so, display the message
+                  item.background === "orange" ? (
+                    <p style={{ color: item.initial }}>
+                      Check store/flux.js scroll to the actions to see the code
+                    </p>
+                  ) : null
+                }
+                <button
+                  className="btn btn-success"
+                  onClick={() => actions.changeColor(index, "orange")}
+                >
+                  Change Color
+                </button>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </div>
 
-    <div className="mb-3">
-      <label for="email" className="form-label">
-        Email
-      </label>
-      <div className="input-group">
-        <input
-          type="text"
-          className="form-control"
-          id="email"
-          placeholder="Enter email"
-        />
+      <div className="container">
+        <h1 className="text-center">Add a new contact</h1>
+        <div className="mb-3">
+          <label for="fullname" className="form-label">
+            Full name
+          </label>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              id="fullname"
+              placeholder="Fullname"
+              onChange={handleInputs}
+            />
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <label for="email" className="form-label">
+            Email
+          </label>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              id="email"
+              placeholder="Enter email"
+              onChange={handleInputs}
+            />
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <label for="phone" className="form-label">
+            Phone
+          </label>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              id="phone"
+              placeholder="Enter phone"
+              onChange={handleInputs}
+            />
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <label for="address" className="form-label">
+            Address
+          </label>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              id="address"
+              placeholder="Enter address"
+              onChange={handleInputs}
+            />
+          </div>
+        </div>
+
+        <br />
+        <div className="d-grid gap-2">
+          <button
+            className="btn btn-primary mb-2"
+            type="button"
+            onClick={handleSave}
+          >
+            Save
+          </button>
+        </div>
+        <Link to="/"> or get back to contacts</Link>
       </div>
-    </div>
-
-    <div className="mb-3">
-      <label for="phone" className="form-label">
-        Phone
-      </label>
-      <div className="input-group">
-        <input
-          type="text"
-          className="form-control"
-          id="phone"
-          placeholder="Enter phone"
-        />
-      </div>
-    </div>
-
-    <div className="mb-3">
-      <label for="address" className="form-label">
-        Address
-      </label>
-      <div className="input-group">
-        <input
-          type="text"
-          className="form-control"
-          id="address"
-          placeholder="Enter address"
-        />
-      </div>
-    </div>
-
-    <br />
-    <div className="d-grid gap-2">
-      <button className="btn btn-primary mb-2" type="button" onClick={() =>actions.allContacts()}>
-        Save
-      </button>
-    </div>
-    <Link to="/"> or get back to contacts</Link>
-  </div>
-</>
+    </>
   );
 };
